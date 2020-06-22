4cbf6c9304c18a22369434848e1d25cfd3bc80ae
Will Badart <badart_william@bah.com>
2020-06-22 08:22:19 -0700

    Upgraded stack LTS and added default.nix

---
+++ default.nix
@@ -0,0 +1,13 @@
+let
+  pkgs = import <nixpkgs> { };
+  compilerVersion = "ghc883"; 
+  compiler = pkgs.haskell.packages."${compilerVersion}";
+in
+  compiler.developPackage {
+    root = ./language-python;
+    modifier = drv:
+      pkgs.haskell.lib.addBuildTools drv (with pkgs.haskellPackages; [
+        cabal-install
+        ghcid
+      ]);
+  }
