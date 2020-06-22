6c048c215ff7fe4a5d5cc36ba3c17a666af74821
Will Badart <badart_william@bah.com>
2020-06-22 08:36:24 -0700

    Switch to defaul.nix from cabal2nix

--- default.nix
+++ default.nix
@@ -1,13 +1,15 @@
-let
-  pkgs = import <nixpkgs> { };
-  compilerVersion = "ghc883"; 
-  compiler = pkgs.haskell.packages."${compilerVersion}";
-in
-  compiler.developPackage {
-    root = ./.;
-    modifier = drv:
-      pkgs.haskell.lib.addBuildTools drv (with pkgs.haskellPackages; [
-        cabal-install
-        ghcid
-      ]);
-  }
+{ mkDerivation, alex, array, base, containers, happy, monads-tf
+, pretty, stdenv, transformers, utf8-string
+}:
+mkDerivation {
+  pname = "language-python";
+  version = "0.5.8";
+  src = ./.;
+  libraryHaskellDepends = [
+    array base containers monads-tf pretty transformers utf8-string
+  ];
+  libraryToolDepends = [ alex happy ];
+  homepage = "http://github.com/bjpop/language-python";
+  description = "Parsing and pretty printing of Python code";
+  license = stdenv.lib.licenses.bsd3;
+}
